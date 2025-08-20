dae8deb62c618ad2f6dbcc1c3b1cafc2a918d2a9
sonms <smsms5676@naver.com>
2025-08-21 01:05:32 +0900

    feat/#3 main - main navigator, bottombar, maintab, screen

---
+++ app/src/main/java/com/example/makersassignment/presentation/main/MainScreen.kt
@@ -0,0 +1,88 @@
+package com.example.makersassignment.presentation.main
+
+import android.app.Activity
+import android.widget.Toast
+import androidx.activity.compose.BackHandler
+import androidx.compose.animation.EnterTransition
+import androidx.compose.animation.ExitTransition
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableLongStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.navigation.compose.NavHost
+import com.example.makersassignment.presentation.auth.navigation.authGraph
+import com.example.makersassignment.presentation.home.navigation.homeGraph
+import com.example.makersassignment.presentation.main.component.MainBottomBar
+import com.example.makersassignment.presentation.mypage.navigation.myPageGraph
+import kotlinx.collections.immutable.toPersistentList
+
+private const val EXIT_MILLIS = 3000L
+
+@Composable
+fun MainScreen(
+    navigator: MainNavigator = rememberMainNavigator(),
+) {
+    val context = LocalContext.current
+    var backPressedTime by remember { mutableLongStateOf(0L) }
+
+    val exitToast = remember {
+        Toast.makeText(context, "한 번 더 누르면 앱이 종료됩니다.", Toast.LENGTH_SHORT)
+    }
+
+    if (navigator.currentTab != null) {
+        BackHandler {
+            val currentTime = System.currentTimeMillis()
+            if (currentTime - backPressedTime <= EXIT_MILLIS) {
+                (context as? Activity)?.finish()
+            } else {
+                backPressedTime = currentTime
+                exitToast.show()
+            }
+        }
+    }
+
+    Scaffold (
+        bottomBar = {
+            MainBottomBar(
+                isVisible = navigator.showBottomBar(),
+                tabs = MainTab.entries.toPersistentList(),
+                currentTab = navigator.currentTab,
+                onTabSelected = navigator::navigate,
+            )
+        },
+        modifier = Modifier
+            .systemBarsPadding()
+            .fillMaxSize()
+    ) { innerPadding ->
+        NavHost(
+            enterTransition = { EnterTransition.None },
+            exitTransition = { ExitTransition.None },
+            popEnterTransition = { EnterTransition.None },
+            popExitTransition = { ExitTransition.None },
+            navController = navigator.navController,
+            startDestination = navigator.startDestination,
+        ) {
+            homeGraph(
+                paddingValues = innerPadding,
+                navigateUp = navigator::navigateUp,
+            )
+
+            myPageGraph(
+                paddingValues = innerPadding,
+                navigateUp = navigator::navigateUp,
+            )
+
+            authGraph(
+                paddingValues = innerPadding,
+                navigateHome = navigator::navigateHomeFromLogin,
+                navigateUp = navigator::navigateUp,
+            )
+        }
+    }
+}
