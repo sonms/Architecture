918af90f71521878a3a9e49217670724d248304b
sonms <smsms5676@naver.com>
2025-08-21 01:07:21 +0900

    feat/#3 auth 구현 - navigation, screen

---
+++ app/src/main/java/com/example/makersassignment/presentation/auth/navigation/AuthNavigation.kt
@@ -0,0 +1,24 @@
+package com.example.makersassignment.presentation.auth.navigation
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+import com.example.makersassignment.core.navigation.MainTabRoute
+import com.example.makersassignment.core.navigation.Route
+import com.example.makersassignment.presentation.auth.SignInRoute
+import kotlinx.serialization.Serializable
+
+fun NavGraphBuilder.authGraph(
+    paddingValues: PaddingValues,
+    navigateUp: () -> Unit,
+    navigateHome: () -> Unit,
+) {
+    composable<Auth> {
+        SignInRoute(
+            navigateHome = navigateHome
+        )
+    }
+}
+
+@Serializable
+data object Auth : Route
