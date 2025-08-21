d9bee7401aa9b8cf952fb36ef3ce9f8f39a88421
sonms <smsms5676@naver.com>
2025-08-22 01:35:33 +0900

    feat/#7 di - network, repository, service Module 각 내용에 맞게 세팅

--- app/src/main/java/com/example/makersassignment/data/di/ServiceModule.kt
+++ app/src/main/java/com/example/makersassignment/data/di/ServiceModule.kt
@@ -1,11 +1,19 @@
 package com.example.makersassignment.data.di
 
+import com.example.makersassignment.data.service.GalleryService
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.create
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object ServiceModule {
-
+    @Provides
+    @Singleton
+    fun provideGalleryService(retrofit: Retrofit): GalleryService =
+        retrofit.create()
 }
