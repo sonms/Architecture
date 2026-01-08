67f46c800b1547a2d76fad5274a5df4f64d4e3d8
minseong-PC <smsdia412@gmail.com>
2026-01-08 17:28:34 +0900

    mod/interface 구조의 변환에 따른 uimodel 수정

--- app/src/main/java/com/example/makersassignment/presentation/home/model/GalleryImageUiModel.kt
+++ app/src/main/java/com/example/makersassignment/presentation/home/model/GalleryImageUiModel.kt
@@ -1,27 +1,9 @@
 package com.example.makersassignment.presentation.home.model
 
+import androidx.compose.runtime.Immutable
 import com.example.makersassignment.domain.entity.GalleryImageEntity
 
+@Immutable
 data class GalleryImageUiModel(
-    val id : String,
-    val author : String,
-    val width : Int,
-    val height : Int,
-    val url : String,
-    val downloadUrl : String
-)
-
-fun GalleryImageEntity.toUiModel() : GalleryImageUiModel {
-    val targetSize = 300
-    val baseUrl = downloadUrl.substringBeforeLast("/").substringBeforeLast("/")
-    val finalUrl = "$baseUrl/$targetSize.jpg"
-
-    return GalleryImageUiModel(
-        id = id,
-        author = author,
-        width = width,
-        height = height,
-        url = url,
-        downloadUrl = finalUrl
-    )
-}
+    val galleryImageUiModel : GalleryImageEntity = GalleryImageEntity.EMPTY
+) : GalleryImageEntity by galleryImageUiModel
