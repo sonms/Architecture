bcc8f2575b3580259bbc6ac5e59f29b1a4a4d3ba
minseong-PC <smsdia412@gmail.com>
2026-01-08 17:28:09 +0900

    mod/prefetch 시 캐싱을 통해 중복 호출 막기

--- app/src/main/java/com/example/makersassignment/data/repositoryimpl/remote/GalleryRepositoryImpl.kt
+++ app/src/main/java/com/example/makersassignment/data/repositoryimpl/remote/GalleryRepositoryImpl.kt
@@ -3,15 +3,34 @@ package com.example.makersassignment.data.repositoryimpl.remote
 import com.example.makersassignment.data.datasource.remote.GalleryDataSource
 import com.example.makersassignment.domain.entity.GalleryImageEntity
 import com.example.makersassignment.domain.repository.GalleryRepository
+import timber.log.Timber
 import javax.inject.Inject
 
 class GalleryRepositoryImpl @Inject constructor(
     private val dataSource: GalleryDataSource
 ) : GalleryRepository {
+    private var prefetchBuffer: Pair<Int, List<GalleryImageEntity>>? = null
+
     override suspend fun getGalleryImageList(
         page: Int,
         limit: Int
     ): Result<List<GalleryImageEntity>> = runCatching {
-        dataSource.getImages(page, limit).map { it.toDomain() }
+        val bufferedData = prefetchBuffer
+
+        if (bufferedData != null && bufferedData.first == page) {
+            prefetchBuffer = null
+            Timber.d("prefetched data -> 스킵")
+            return Result.success(bufferedData.second)
+        }
+
+        return try {
+            val response = dataSource.getImages(page, limit)
+            val entities = response.map { it.toDomain() }
+            prefetchBuffer = page to entities
+
+            Result.success(entities)
+        } catch (e: Exception) {
+            Result.failure(e)
+        }
     }
 }
