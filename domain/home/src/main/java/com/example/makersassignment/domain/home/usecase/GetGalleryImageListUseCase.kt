41eb15e70f5982a6538fc92cc877ff3cc983f159
sonms <smsms5676@naver.com>
2026-04-23 17:25:29 +0900

    refactor: GetGalleryImageListUseCase의 반환 타입을 Result<List<GalleryImageEntity>>로 명시

--- domain/home/src/main/java/com/example/makersassignment/domain/home/usecase/GetGalleryImageListUseCase.kt
+++ domain/home/src/main/java/com/example/makersassignment/domain/home/usecase/GetGalleryImageListUseCase.kt
@@ -1,10 +1,12 @@
 package com.example.makersassignment.domain.home.usecase
 
+import com.example.makersassignment.domain.home.entity.GalleryImageEntity
 import com.example.makersassignment.domain.home.repository.GalleryRepository
 import javax.inject.Inject
 
 class GetGalleryImageListUseCase @Inject constructor(
     private val repository: GalleryRepository
 ) {
-    suspend operator fun invoke(page : Int, limit : Int) = repository.getGalleryImageList(page, limit)
+    suspend operator fun invoke(page: Int, limit: Int): Result<List<GalleryImageEntity>> =
+        repository.getGalleryImageList(page, limit)
 }
